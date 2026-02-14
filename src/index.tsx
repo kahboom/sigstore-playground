653b80c8b38c98f0a534ff62ade68b20bcf972b3
kahboom <rachelyordan@gmail.com>
2026-02-14 15:34:45 +0000

    fix: add dev title for convenience

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,11 @@ import { render } from 'solid-js/web';
 import App from './App';
 import './styles/global.css';
 
+// Update document title for development mode
+if (import.meta.env.DEV) {
+  document.title = '[DEV] Sigstore Playground';
+}
+
 const root = document.getElementById('root');
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
